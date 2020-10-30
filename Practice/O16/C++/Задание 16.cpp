2f0e582ee6eb441f2a08f07fbe1987378c1a8f54
Никита Нестеренко <nsn2002@outlook.com>
2020-10-30 19:23:50 +0300

    Задание 16 патерн

--- Practice/O16/C++/Задание 16.cpp
+++ Practice/O16/C++/Задание 16.cpp
@@ -1,20 +1,29 @@
 ﻿#include <iostream>
 using namespace std;
 
+const string PATTERN = "a???55661";
+
+string check(string ticket) {
+	for (int i = 0; i < PATTERN.size(); i++) {
+		if ('?' != PATTERN[i] && ticket[i] != PATTERN[i]) {
+			return "";
+		}
+	}
+	return ticket + ' ';
+}
+
 int main() {
 	setlocale(LC_ALL, "ru");
 
 	int n;
 	cout << "Введите колличество билетов Суслика: " << endl;
 	cin >> n;
-	string tickets;
+	string ticket;
 	string symbols = "";
 	cout << "Введите номера билетов через пробел: " << endl;
 	for (int i = 0; i < n; i++) {
-		cin >> tickets;
-		if ((tickets[0] == 'a') && (tickets[4] == '5') && (tickets[5] == '5') && (tickets[6] == '6') && (tickets[7] == '6') && (tickets[8] == '1')) {
-			symbols = symbols + tickets + ' ';
-		}
+		cin >> ticket;
+		symbols += check(ticket);
 	}
 	if (symbols == "")
 		cout << -1;
