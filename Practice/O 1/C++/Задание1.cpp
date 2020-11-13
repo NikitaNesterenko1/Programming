fef151534567dba89f32f1f719782bf297c119bb
Никита Нестеренко <nsn2002@outlook.com>
2020-11-13 13:15:30 +0300

    Задание 23

--- Practice/O 1/C++/Задание1.cpp
+++ Practice/O 1/C++/Задание1.cpp
@@ -1,11 +1,15 @@
 ﻿
 
 #include <iostream>
+using namespace std;
+
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    std::cout << "Результат выражения 2+2*2 =  " << 2 + (2 * 2);
+	int a = 5;
+	int b = 6;
+	cout << ((a > b) ? b : a);
 }
 
---
+++ Practice/O23/C++/Задание 23.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <iomanip>  //std::setprecision
+
+#include "sin.hpp"
+#include "factorial.hpp"
+#include "combinations.hpp"
+{
+double const PI = 3.1415926;
+
+int main() {
+    std::cout << "n\tn!" << std::endl;
+    for (int64_t i = 1; i <= 10; ++i) {
+        std::cout << i << "\t" << factorial(i) << std::endl;
+    }   
+    std::cout << std::endl;
+
+
+}
+
+
+
+}
